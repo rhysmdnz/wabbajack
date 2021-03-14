3c489d86387dad469c6cb9cb475ba4a40067a08c
Timothy Baldridge <tbaldridge@gmail.com>
2021-03-14 14:54:07 -0600

    Gallery populates

---
+++ Wabbajack.App/Controls/GalleryItemVM.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI.Fody.Helpers;
+using Wabbajack.Lib;
+
+namespace Wabbajack.App.Controls
+{
+    public class GalleryItemVM : ViewModel
+    {
+        [Reactive] public string Title { get; set; } = "";
+
+    }
+}
--- Wabbajack.App/Screens/ModListGalleryVM.cs
+++ Wabbajack.App/Screens/ModListGalleryVM.cs
@@ -1,9 +1,41 @@
-﻿using Wabbajack.Lib;
+﻿using System;
+using System.Threading.Tasks;
+using ReactiveUI.Fody.Helpers;
+using Wabbajack.Lib;
+using Wabbajack.Lib.ModListRegistry;
 
 namespace Wabbajack.App.Screens
 {
     public class ModListGalleryVM : ViewModel
     {
-        
+        [Reactive] 
+        public ModlistMetadata[] ModLists { get; set; } = Array.Empty<ModlistMetadata>();
+
+        [Reactive] public LoadingStatus Status { get; set; } = LoadingStatus.Loading;
+        public ModListGalleryVM()
+        {
+            var tsk = ReloadLists();
+        }
+
+        public async Task ReloadLists()
+        {
+            Status = LoadingStatus.Loading;
+            try
+            {
+                ModLists = (await ModlistMetadata.LoadFromGithub()).ToArray();
+                Status = LoadingStatus.Ready;
+            }
+            catch(Exception ex)
+            {
+                Status = LoadingStatus.Errored;
+            }
+        }
+
+        public enum LoadingStatus
+        {
+            Ready,
+            Loading,
+            Errored
+        }
     }
 }
